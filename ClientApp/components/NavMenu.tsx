161650f1c0d56bd1430578b3a6f8f1007110315d
Sarah Tammenga <0881357@hr.nl>
2017-09-26 14:24:22 +0200

    Home NavMenu en tsconfig aangepast

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -12,7 +12,7 @@ export class NavMenu extends React.Component<{}, {}> {
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>Test</Link>
+                    <Link className='navbar-brand' to={ '/' }>SepatuS</Link>
                 </div>
                 <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
@@ -24,12 +24,12 @@ export class NavMenu extends React.Component<{}, {}> {
                         </li>
                         <li>
                             <NavLink to={ '/counter' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-education'></span> Counter
+                                <span className='glyphicon glyphicon-th-list'></span> Women
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to={ '/fetchdata' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Fetch data
+                            <NavLink to={'/fetchdata' } activeClassName='active'>
+                                <span className='glyphicon glyphicon-th-list'></span> Men
                             </NavLink>
                         </li>
                     </ul>
