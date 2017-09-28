334242e2c19be368e41ec9bf7960ebdf6dca4b01
Mediamarkt <0925178@hr.nl>
2017-09-28 12:09:12 +0200

    shoes divided in Man and Woman

--- ClientApp/components/VrouwSchoenen.tsx
+++ ClientApp/components/VrouwSchoenen.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import * as Types from './custom_types'
 import { Link, NavLink } from 'react-router-dom';
-type SchoenenState = {}
+type VrouwSchoenenState = {}
 
-export class Schoenen extends React.Component<RouteComponentProps<{}>, SchoenenState> {
+export class VrouwSchoenen extends React.Component<RouteComponentProps<{}>, VrouwSchoenenState> {
     constructor() {
         super();
         this.state = {}
@@ -12,15 +12,7 @@ export class Schoenen extends React.Component<RouteComponentProps<{}>, SchoenenS
 
     public render() {
 
-        return <div> <h1> Alle Schoenen</h1>
-            <h3>{Types.Shoes0.Name} </h3>
-            <img src={Types.Shoes0.Image} width="250" height="250" />
-            <h4>€{Types.Shoes0.Price}.00 </h4>
-            <li>
-                <NavLink to={'/detailpagina'} activeClassName='active'>
-                    <button> Lees meer!</button>
-                </NavLink>
-            </li>
+        return <div> <h1> Vrouwen Schoenen</h1>
 
             <h3>{Types.Shoes1.Name} </h3>
             <img src={Types.Shoes1.Image} width="250" height="250" />
@@ -34,15 +26,6 @@ export class Schoenen extends React.Component<RouteComponentProps<{}>, SchoenenS
             <h3>{Types.Shoes2.Name} </h3>
             <img src={Types.Shoes2.Image} width="250" height="250" />
             <h4>€{Types.Shoes2.Price}.00 </h4>
-            <li>
-                <NavLink to={'/detailpagina'} activeClassName='active'>
-                    <button> Lees meer!</button>
-                </NavLink>
-            </li>
-
-            <h3>{Types.Shoes3.Name} </h3>
-            <img src={Types.Shoes3.Image} width="250" height="250" />
-            <h4>€{Types.Shoes3.Price}.00 </h4>
             <li>
                 <NavLink to={'/detailpagina'} activeClassName='active'>
                     <button> Lees meer!</button>
