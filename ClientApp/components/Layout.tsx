d202b6b766d05c0a920e900c8e018f5c79444962
Mischa van Dalen <omekeki@users.noreply.github.com>
2017-10-03 12:42:54 +0200

    initial layout markup

--- ClientApp/components/Layout.tsx
+++ ClientApp/components/Layout.tsx
@@ -7,15 +7,14 @@ export interface LayoutProps {
 
 export class Layout extends React.Component<LayoutProps, {}> {
     public render() {
-        return <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-3'>
-                    <NavMenu />
-                </div>
-                <div className='col-sm-9'>
+        return <div className='main_container'>
+            <div
+            {/* <NavMenu />  */}
+                
+                
                     { this.props.children }
-                </div>
-            </div>
+                
+           
         </div>;
     }
 }
