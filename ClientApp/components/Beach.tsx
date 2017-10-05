5eb051b9ef192a5e2632ceb094c65b011175b1f0
Sarah Tammenga <0881357@hr.nl>
2017-10-05 14:52:32 +0200

    Schoenen toegevoegd in Casual, BEach, Formal. Namen van de components veranderd en de verwijzing naar de pages geplaatst in routes.

--- ClientApp/components/Beach.tsx
+++ ClientApp/components/Beach.tsx
@@ -2,9 +2,9 @@ import * as Types from './custom_types'
 import { Link, NavLink } from 'react-router-dom';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-type CasualSchoenenState = {}
+type BeachSchoenenState = {}
 
-export class Casual extends React.Component<RouteComponentProps<{}>, CasualSchoenenState> {
+export class Beach extends React.Component<RouteComponentProps<{}>, BeachSchoenenState> {
     constructor() {
         super();
         this.state = {}
@@ -12,43 +12,40 @@ export class Casual extends React.Component<RouteComponentProps<{}>, CasualSchoe
 
     public render() {
 
-        return <div> <h1> Casual </h1>
-            <h3>{Types.Shoes0.Name} </h3>
-            <img src={Types.Shoes0.Image} width="250" height="250" />
-            <h4>€{Types.Shoes0.Price}.00 </h4>
+        return <div> <h1> Schoenen voor het strand</h1>
+            <h3>{Types.Shoes7.Name} </h3>
+            <img src={Types.Shoes7.Image} width="250" height="250" />
+            <h4>€{Types.Shoes7.Price}.00 </h4>
             <li>
                 <NavLink to={'/detailpagina'} activeClassName='active'>
                     <button> Lees meer!</button>
                 </NavLink>
             </li>
 
-            <h3>{Types.Shoes4.Name} </h3>
-            <img src={Types.Shoes4.Image} width="250" height="250" />
-            <h4>€{Types.Shoes4.Price}.00 </h4>
+            <h3>{Types.Shoes8.Name} </h3>
+            <img src={Types.Shoes8.Image} width="250" height="250" />
+            <h4>€{Types.Shoes8.Price}.00 </h4>
             <li>
                 <NavLink to={'/detailpagina'} activeClassName='active'>
                     <button> Lees meer!</button>
                 </NavLink>
             </li>
-
-            <h3>{Types.Shoes5.Name} </h3>
-            <img src={Types.Shoes5.Image} width="250" height="250" />
-            <h4>€{Types.Shoes5.Price}.00 </h4>
+            <h3>{Types.Shoes9.Name} </h3>
+            <img src={Types.Shoes9.Image} width="250" height="250" />
+            <h4>€{Types.Shoes9.Price}.00 </h4>
             <li>
                 <NavLink to={'/detailpagina'} activeClassName='active'>
                     <button> Lees meer!</button>
                 </NavLink>
             </li>
-            <h3>{Types.Shoes6.Name} </h3>
-            <img src={Types.Shoes6.Image} width="250" height="250" />
-            <h4>€{Types.Shoes6.Price}.00 </h4>
+            <h3>{Types.Shoes10.Name} </h3>
+            <img src={Types.Shoes10.Image} width="250" height="250" />
+            <h4>€{Types.Shoes10.Price}.00 </h4>
             <li>
                 <NavLink to={'/detailpagina'} activeClassName='active'>
                     <button> Lees meer!</button>
                 </NavLink>
             </li>
-
-
         </div>
     }
 
--- ClientApp/components/custom_types.ts
+++ ClientApp/components/custom_types.ts
@@ -62,3 +62,135 @@ export let Shoes3: Schoenen =
     Description: "Materiaal buitenlaag:Leer, Voering:Leer, Binnenzool:Leer, Zool:Kunststof/ Leer, Voeringdikte:Zeer licht gevoerd",
     Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/G7/81/2A/01/VO/11/G7812A01V-O11@12.jpg",
 }
+export let Shoes4: Schoenen =
+{
+    id: 5,
+    Name: "CUPSOLE ALPINE OX",
+    Price: 108,
+    Size: 42,
+    Category: "Heren",
+    Brand: "Timberland",
+    Color: "zwart",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/TI/11/2B/02/DQ/11/TI112B02D-Q11@12.jpg",
+}
+export let Shoes5: Schoenen =
+{
+    id: 6,
+    Name: "MAXWELL",
+    Price: 100,
+    Size: 46,
+    Category: "Heren",
+    Brand: "Tommy Hilfiger",
+    Color: "zwart",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/TO/11/2O/00/5K/11/TO112O005-K11@12.jpg",
+}
+export let Shoes6: Schoenen =
+{
+    id: 7,
+    Name: "ZIGN",
+    Price: 100,
+    Size: 46,
+    Category: "Heren",
+    Brand: "Zign",
+    Color: "zwart",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/ZI/11/2B/A1/2A/11/ZI112BA12-A11@12.jpg",
+}
+export let Shoes7: Schoenen =
+{
+    id: 8,
+    Name: "FANNIING",
+    Price: 50,
+    Size: 42,
+    Category: "Heren",
+    Brand: "Reef",
+    Color: "zwart",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/RE/21/2H/00/SQ/11/RE212H00S-Q11@12.jpg",
+}
+export let Shoes8: Schoenen =
+{
+    id: 9,
+    Name: "DOBRY",
+    Price: 50,
+    Size: 42,
+    Category: "Heren",
+    Brand: "ZIGN",
+    Color: "bruin",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://www.zalando.nl/zign-teensandalen-dobry-zi112ga03-o13.html",
+}
+export let Shoes9: Schoenen =
+{
+    id: 10,
+    Name: "CROCOBAND",
+    Price: 25,
+    Size: 42,
+    Category: "Heren",
+    Brand: "Crocs",
+    Color: "zwart",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/CR/41/2E/00/18/02/CR412E001-802@19.jpg",
+}
+export let Shoes10: Schoenen =
+{
+    id: 11,
+    Name: "TOP MIX",
+    Price: 16,
+    Size: 41,
+    Category: "Heren",
+    Brand: "Havaianas",
+    Color: "donker blauw",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/HA/11/5B/00/7K/14/HA115B007-K14@12.jpg",
+}
+export let Shoes11: Schoenen =
+{
+    id: 12,
+    Name: "Veterschoenen",
+    Price: 90,
+    Size: 41,
+    Category: "Heren",
+    Brand: "Pier One",
+    Color: "zwart",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/PI/91/2A/A5/BQ/11/PI912AA5B-Q11@12.jpg",
+}
+export let Shoes12: Schoenen =
+{
+    id: 13,
+    Name: "Instappers",
+    Price: 48,
+    Size: 41,
+    Category: "Heren",
+    Brand: "Zign",
+    Color: "beige",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/ZI/11/2C/A0/EB/13/ZI112CA0E-B13@12.jpg",
+}
+export let Shoes13: Schoenen =
+{
+    id: 14,
+    Name: "Veterschoenen",
+    Price: 70,
+    Size: 45,
+    Category: "Heren",
+    Brand: "Zign",
+    Color: "bruin",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/ZI/11/2A/0D/EO/11/ZI112A0DE-O11@12.jpg",
+}
+export let Shoes14: Schoenen =
+{
+    id: 15,
+    Name: "Veterschoenen",
+    Price: 35,
+    Size: 45,
+    Category: "Heren",
+    Brand: "Pier One",
+    Color: "zwart",
+    Description: "Materiaal buitenlaag:Leer, Voering:Combinatie van leren- en textielvoering, Binnenzool:Textiel, Zool:Kunststof, Voeringdikte:Zeer licht gevoerd",
+    Image: "https://mosaic01.ztat.net/vgs/media/pdp-gallery/PI/91/2A/A4/5Q/11/PI912AA45-Q11@12.jpg",
+}
