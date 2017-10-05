1cb608b108608ade522915feb3a349fecc6c2e7a
Marco den Hollander <admin@xara.moe>
2017-10-05 15:12:49 +0200

    Change Confirmation to bootstrap

--- ClientApp/components/Confirmation.tsx
+++ ClientApp/components/Confirmation.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import * as Types from './custom_types'
 import { Link, NavLink } from 'react-router-dom';
+import {Image,
+        Grid,
+        Row,
+        Col,
+        Thumbnail,
+        Button,
+    Media} from 'react-bootstrap';
 type SchoenenState = {}
 
 var buttonStyle = {
@@ -10,12 +17,23 @@ var buttonStyle = {
 
 export class Confirmation extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
-        return <div className='confirm_placeholder'> <h1>Uw order</h1>
-        <h3>{Types.Shoes1.Name} </h3>
-            <h4>aantal: 1</h4>
-            <img src={Types.Shoes1.Image} width="250" height="250" />
-            <h4>€{Types.Shoes1.Price}.00 </h4>
-            <a href="/" className="btn btn-info" style={buttonStyle} role="button">Back to Homepage</a>
-         </div>
+        return <Row>
+        <Col xs={2} md={2}></Col>
+        <Col xs={2} md={2}>
+        <Media>
+     <Media.Left>
+        <img width={128} height={128} src={Types.Shoes1.Image} alt="Image"/>
+      </Media.Left>
+      <Media.Body>
+        <Media.Heading><b>{Types.Shoes1.Name}</b></Media.Heading>
+        <h4>aantal: 1</h4>
+        <h4>€{Types.Shoes1.Price}.00 </h4>
+      </Media.Body>
+      <br/>
+      <br/>
+      <Button href="/"bsStyle="primary" bsSize="large">Back to Homepage</Button>
+    </Media>
+    </Col>
+    </Row>
     }
 }
