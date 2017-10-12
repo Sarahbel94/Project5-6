0008a3f77a4df82c06b984023bd7ce4dded53983
Mischa van Dalen <omekeki@users.noreply.github.com>
2017-10-12 15:38:44 +0200

    fixed menu

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -5,7 +5,7 @@ import { Home } from './components/Home';
 import { ManSchoenen } from './components/ManSchoenen';
 import { VrouwSchoenen } from './components/VrouwSchoenen';
 import { Schoenen } from './components/Schoenen';
-import { DetailPagina } from './components/DetailPagina';
+// import { DetailPagina } from './components/DetailPagina';
 import { Confirmation } from './components/Confirmation';
 
 import { Wishlist } from './components/Wishlist';
@@ -48,7 +48,7 @@ export const routes =
     <Route path='/ManSchoenen' component={ ManSchoenen } />
     <Route path='/schoenen' component={ Schoenen } />
     <Route path='/confirmation' component={ Confirmation } />
-    <Route path='/detailpagina' component={ DetailPagina } />
+    {/* <Route path='/detailpagina' component={ DetailPagina } /> */}
    
     <Route path='/wishlist' component={ Wishlist }/> 
     
