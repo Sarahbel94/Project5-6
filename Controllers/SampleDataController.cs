4ded785f5b1365d07e2a389e018b1a071a3c3eae
safiaAa <0935605@hr.nl>
2017-10-22 21:42:12 +0200

    tesst

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Test.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]")]  
     public class SampleDataController : Controller
     {
         private static string[] Summaries = new[]
